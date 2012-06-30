cf3375a89c39344651a3db40682b4979b0bdbe13
Ioannis Papagiannis <jpapayan@gmail.com>
2012-06-30 20:53:33 +0100

    Cleaup and v1.2.2

--- src/com/papagiannis/tuberun/OysterActivity.java
+++ src/com/papagiannis/tuberun/OysterActivity.java
@@ -13,7 +13,6 @@ import android.content.DialogInterface.OnCancelListener;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
